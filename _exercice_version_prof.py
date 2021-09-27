b8eca059e1567d16ed6929445fb5a12f7cc446cb
chosson <chosson@users.noreply.github.com>
2021-09-27 15:03:38 -0400

    Added answers

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -3,22 +3,56 @@
 
 import math
 
+
 def get_num_letters(text):
-	return 0
+	num_letters = 0
+	for chr in text:
+		num_letters += int(chr.isalnum())
+	return num_letters
+	# Approche avec compréhension de liste où on compte le nombre d'élément de la liste
+	#return len([None for chr in text if chr.isalnum()])
 
 def get_word_length_histogram(text):
-	return [0]
+	histogram = [0]
+	for word in text.split():
+		length = get_num_letters(word)
+		if length >= len(histogram):
+			histogram += [0] * (length - len(histogram) + 1)
+		histogram[length] += int(length != 0)
+
+	return histogram
 
 def format_histogram(histogram):
 	ROW_CHAR = "*"
 
-	return ""
+	# Approche avec compréhension de liste
+	alignment = len(str(len(histogram) - 1))
+	return "\n".join([f"{i : >{alignment}} {ROW_CHAR * elem}" for i, elem in enumerate(histogram) if i != 0])
+
+	# Approche avec boucles
+	#result = ""
+	#for i, elem in enumerate(histogram):
+	#	if i == 0:
+	#		continue
+	#	result += f"{i : >{alignment}} {ROW_CHAR * elem}" + "\n"
+	#return result
 
 def format_horizontal_histogram(histogram):
 	BLOCK_CHAR = "|"
 	LINE_CHAR = "¯"
 
-	return ""
+	height = max(histogram)
+
+	result = ""
+	for i in range(height - 1, -1, -1):
+		# Compréhension de liste pour chacune des lignes
+		result += "".join([BLOCK_CHAR if elem >= i + 1 else " " for elem in histogram[1:]]) + "\n"
+	# Ajout de la dernière ligne
+	result += LINE_CHAR * len(histogram)
+	return result
+
+	# Solution sur une ligne avec compréhension de liste imbriquée. Pas vraiment plus clair, non? Moins de ligne ne veut pas nécessairement dire plus élégant.
+	#return "\n".join(["".join([(BLOCK_CHAR if col >= i + 1 else " ") for col in histogram[1:]]) for i in range(height - 1, -1, -1)]) + "\n" + LINE_CHAR * (len(histogram) - 1)
 
 
 if __name__ == "__main__":
